f31e24e5033a1348c7376a296162bca0149c6119
hanga.lagoue <wilfriedhanga5@gmail.com>
2024-07-30 13:36:00 +0100

    feat

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,19 +1,17 @@
 <template>
   <section class="p-10 bg-gradient-to-tr from-black to-gray-400 min-h-screen">
     <section class="flex bg-white h-[calc(100vh-200px)] rounded-xl w-[90%] mx-auto">
-      <section class="flex flex-col gap-8 w-[44%] pt-20 items-center">
-        
-        <h1 class="text-5xl flex flex-col gap-2 font-bold text-blue-500">
+      <section class="flex flex-col gap-8 w-[44%] pt-20 px-10">
+        <h1 class="text-5xl flex flex-col gap-2 font-bold text-indigo-500">
             <span>DEAR USER</span>
-            <span>WELCOME TO WORKMAN</span>
+            <span>WELCOME TO <b class="bg-black text-white">WORKMAN</b></span>
         </h1>
-        <div class="flex flex-col">
-            <!-- <span>Connect with skills</span> -->
-            <p>
-                
-            </p>
+        <div class="text-lg font-bold">
+            We are dedicated to simplify the process of finding, affordable, accessible an, reliable skilled 
+            technicians to solve all your daily basic technical faults, from the comfort of your various homes
+            with just a click of a button.
         </div>
-
+        <h3 class="text-3xl font-bold">You be <strong class="bg-indigo-700 text-white">workman?</strong></h3>
       </section>
       <section
         class="grow bg-no-repeat bg-cover bg-[url(@/assets/font.jpeg)] rounded-none"
